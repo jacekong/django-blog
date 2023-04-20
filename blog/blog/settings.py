2d1c30f2356e6f733b2049ee8c94edc6e7845296
JaceKong <78537442+jacekong@users.noreply.github.com>
2023-04-21 02:56:31 +0800

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 MEDIA_URL = 'image/'
 MEDIA_ROOT = BASE_DIR / 'static'
 
