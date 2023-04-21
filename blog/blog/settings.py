ecf431b7f6cdcc7213d5d0d7fec5c7ad540ce564
JaceKong <78537442+jacekong@users.noreply.github.com>
2023-04-22 03:04:26 +0800

    Update settings.py

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 from datetime import timedelta
 
@@ -140,9 +140,9 @@ STATIC_URL = 'static/'
 #     BASE_DIR / "static"
 # ]
 MEDIA_URL = 'image/'
-MEDIA_ROOT = BASE_DIR / 'static'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'image/')
 
-import os
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # ckeditor
